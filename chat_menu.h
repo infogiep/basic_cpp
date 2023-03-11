1fb6598fa70fcfaf02482b79f78671ca2a81aee2
infogiep <122611217+infogiep@users.noreply.github.com>
2023-03-11 12:44:55 +0300

    Add files via upload

---
+++ chat_menu.h
@@ -0,0 +1,14 @@
+#pragma once
+class chat_menu
+{
+public:
+	//bool start_ = false;
+	//int i=0;
+//public:
+	void start();
+	void chat_begin();
+	void chat_user();
+
+};
+
+//class enter_chat;
---
+++ enter_chat.h
@@ -0,0 +1,24 @@
+#pragma once
+class enter_chat
+{
+	//enter_chat();
+	//enter_chat(string& g_name_from);
+	//std::string login_;
+	//std::string password_;
+	//std::string name_;
+
+public:
+	/*
+	enter_chat()
+	{};
+	enter_chat(std::string& user_login; std::string& user_password; std::string& user_name) :  _user_login(user_login), _user_password(user_password), _user_name(user_name)
+	*/
+
+
+	//string login, password, name;
+
+
+	void enter_user();
+	//string g_name_from;
+};
+
